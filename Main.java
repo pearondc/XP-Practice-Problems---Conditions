0c14f7c642ca3dc75aa9809f555835f92c0581ef
pearondc <89395893+pearondc@users.noreply.github.com>
2021-09-20 21:25:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+  /*
+  //Problem 1
+  System.out.println("Please enter 3 whole numbers, your first number:");
+  Scanner s = new Scanner(System.in);
+  int firstNum = s.nextInt();
+  int max = firstNum;
+  System.out.println("Please enter your second number");
+  int secondNum = s.nextInt();
+  if (secondNum > firstNum) {
+    max = secondNum;
+  }
+ System.out.println("Please enter your third number");
+  int thirdNum = s.nextInt();
+  if (thirdNum > firstNum && thirdNum > secondNum) {
+    max = thirdNum;
+  }
+  System.out.println("The largest number of the three you entered is " + max);
+  */
+
+  /*
+  //Problem 2
+  System.out.println("Please enter 2 whole numbers. First number:");
+  Scanner s = new  Scanner(System.in);
+  int firstNum = s.nextInt();
+   System.out.println("Second number:");
+  int secondNum = s.nextInt();
+
+  if(firstNum <= secondNum){
+    while(firstNum <= secondNum){
+      System.out.println(firstNum);
+      firstNum++;
+    }
+  }
+  else {
+    while(firstNum >= secondNum){
+      System.out.println(secondNum);
+      secondNum++;
+    }
+  } //this else statement is to capture and calculate should the user enter the larger number first
+  */
+
+  /*
+  //Problem 3
+  System.out.println("Please enter a year as 4 digits (i.e., 2021):");
+  Scanner s = new  Scanner(System.in);
+  int year = s.nextInt();
+  double divFour = year % 4;
+  double divHundred = year % 100;
+  double divFourHundred = year % 400;
+  if (year < 0){
+    System.out.println("The year cannot be negative. Please enter a different year");
+  }
+
+  if(year <= 999){
+     System.out.println("The year must have 4 digits. Please enter a different year");
+  }
+
+  if (divFour == 0 && divHundred > 0) {
+      System.out.println("The year you entered is a leap year!");
+  }
+
+  else if(divHundred == 0 && divFourHundred == 0) {
+      System.out.println("The year you entered is a leap year!");
+  }
+
+  else {
+    System.out.println("The year you entered is not a leap year!");
+  }
+  */
+
+  //Problem 4
+  
+  }
+}
